6d2cd8c1e3a4ce92f3248a216a7e047025c9fe1e
MagiX7 <DavidGonzalez2001-9@hotmail.com>
2020-11-26 12:04:39 +0100

    Body class

---
+++ Game/Source/bhBody.h
@@ -0,0 +1,116 @@
+#pragma once
+
+#include "bhVec2.h"
+
+
+class bhBody
+{
+private:
+	bhVec2 position;
+	bhVec2 linearV;
+	float angularV;
+	bhVec2 acceleration;
+	bhVec2 force;
+	float mass;
+	float bodyGravity;
+	float angle;
+
+public:
+	// Default constructor that sets all the variables to default
+	bhBody()
+	{
+		position = bhVec2(0.0f, 0.0f);
+		linearV = bhVec2(0.0f, 0.0f);
+		angularV = 0.0f;
+		acceleration = bhVec2(0.0f, 0.0f);
+		force = bhVec2(0.0f, 0.0f);
+		mass = 0.0f;
+		bodyGravity = 0.0f;
+		angle = 0.0f;
+	}
+
+	~bhBody() {};
+
+	inline void AddForce(bhVec2 f)
+	{
+		force += f;
+	}
+
+	// ===================================================
+	//					Setters
+	// ===================================================
+
+	// Set body position 
+	inline void SetPosition(bhVec2 pos)
+	{
+		position = pos;
+	}
+	inline void SetPosition(float x, float y)
+	{
+		position.x = x;
+		position.y = y;
+	}
+
+	// Set body linear speed
+	inline void SetLinearVelocity(bhVec2 v)
+	{
+		linearV = v;
+	}
+	inline void SetLinearVelocity(float x, float y)
+	{
+		linearV.x = x;
+		linearV.y = y;
+	}
+
+	// Set body angluar speed
+	inline void SetAngularVelocity(float v)
+	{
+		angularV = v;
+	}
+	
+	// ===================================================
+	//					Getters
+	// ===================================================
+
+	// Get the body position
+	inline bhVec2 GetPosition()const
+	{
+		return position;
+	}
+	
+	// Get the body linear velocity
+	inline bhVec2 GetLinearVelocity() const
+	{
+		return linearV;
+	}
+
+	// Get the body angular velocity
+	inline float GetAngularVelocity() const
+	{
+		return angularV;
+	}
+
+	// Get the body acceleration
+	inline bhVec2 GetAcceleration() const
+	{
+		return acceleration;
+	}
+
+	// Get body mass
+	inline float GetBodyMass() const
+	{
+		return mass;
+	}
+
+	//Get gravity on the body;
+	inline float GetBodyGravity() const
+	{
+		return bodyGravity;
+	}
+
+	// Get the body angle
+	inline float GetBodyAngle() const
+	{
+		return angle;
+	}
+};
---
+++ Game/Source/bhVec2.h
@@ -0,0 +1,53 @@
+#pragma once
+
+class bhVec2
+{
+public:
+	float x = 0;
+	float y = 0;
+
+public:
+	bhVec2() {};
+
+	bhVec2(float newX, float newY)
+	{
+		x = newX;
+		y = newY;
+	}
+
+	// Set new values for the vector
+	void SetVector(float newX, float newY)
+	{
+		x = newX;
+		y = newY;
+	}
+
+	// Ngeate a vector
+	bhVec2 Negate(bhVec2 v)
+	{
+		v.x = -v.x; 
+		v.y = -v.y;
+	}
+
+	// Vector addition
+	void operator += (const bhVec2 v)
+	{
+		x += v.x;
+		y += v.y;
+	}
+
+	// Vector substraction
+	void operator -= (const bhVec2 v)
+	{
+		x -= v.x;
+		y -= v.y;
+	}
+
+	// Multply a vector by a scalar
+	void operator *= (float a)
+	{
+		x *= a;
+		y *= a;
+	}
+
+};
