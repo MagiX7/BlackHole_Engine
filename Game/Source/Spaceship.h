5a4a9651783b5f4ef3b04a971a29d660e9b56199
Denisdrk6 <denisdsuniversidad@hotmail.com>
2021-01-01 19:15:37 +0100

    spaceship heather updated

--- Game/Source/Spaceship.h
+++ Game/Source/Spaceship.h
@@ -22,15 +22,29 @@ public:
 		return body;
 	}
 
+	void AddHealth(float h) { health += h; }
+	void AddFuel(float f) { fuel += f; }
+	void AddAmmo(int a) { ammo += a; }
+	void LaunchMissile()
+	{
+		bhBody* missile;
+		missile->SetPosition(this->body->GetPosition());
+		missile->SetLinearVelocity(bhVec2(this->body->GetLinearVelocity().x * 1.5f, this->body->GetLinearVelocity().y * 1.5f ));
+	}
+
+
 public:
 	float health;
 	float fuel;
+	int ammo = 3;
 
 private:
 	
 	SDL_Texture* texture;
 	Animation* currentAnim;
 	Animation idleAnim;
+
+
 	
 	//float r; // for collision detection
 
